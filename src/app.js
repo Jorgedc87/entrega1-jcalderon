d1ac198b2c754f9614cd01bae49f2a511443fef0
Jorge Calderon <jorge.calderon@uosolutions.com>
2025-02-23 23:43:14 -0300

    fix path of authMiddleware

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const productsRouter=require('./routes/products.router.js');
 const cartRouter=require('./routes/cart.router.js');
-const authMiddleware = require('./middleware/auth.middleware.js');
+const authMiddleware = require('./middlewares/auth.middleware.js');
 const PORT=8080;
 
 app.use(express.json());
